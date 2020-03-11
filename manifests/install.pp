05a9c08f56159086cfef74739840e2ade25e6771
Nicolas Berens <nicolas.berens@betrolltes-wohnen.de>
2020-03-11 18:54:14 +0100

    clean variable scope

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,19 +1,19 @@
-class php_fpm::install(
-  $ensure = $php_fpm::params::ensure,
-  $pid = $php_fpm::params::pid,
-  $error_log = $php_fpm::params::error_log,
-  $syslog_facility = $php_fpm::params::syslog_facility,
-  $syslog_ident = $php_fpm::params::syslog_ident,
-  $log_level = $php_fpm::params::log_level,
+class php_fpm::install (
+  $ensure                      = $php_fpm::params::ensure,
+  $pid                         = $php_fpm::params::pid,
+  $error_log                   = $php_fpm::params::error_log,
+  $syslog_facility             = $php_fpm::params::syslog_facility,
+  $syslog_ident                = $php_fpm::params::syslog_ident,
+  $log_level                   = $php_fpm::params::log_level,
   $emergency_restart_threshold = $php_fpm::params::emergency_restart_threshold,
-  $emergency_restart_interval = $php_fpm::params::emergency_restart_interval,
-  $process_control_timeout = $php_fpm::params::process_control_timeout,
-  $process_max = $php_fpm::params::process_max,
-  $daemonize = $php_fpm::params::daemonize,
-  $rlimit_files = $php_fpm::params::rlimit_files,
-  $rlimit_core = $php_fpm::params::rlimit_core,
-  $events_mechanism = $php_fpm::params::events_mechanism,
-  )  inherits php_fpm::params {
+  $emergency_restart_interval  = $php_fpm::params::emergency_restart_interval,
+  $process_control_timeout     = $php_fpm::params::process_control_timeout,
+  $process_max                 = $php_fpm::params::process_max,
+  $daemonize                   = $php_fpm::params::daemonize,
+  $rlimit_files                = $php_fpm::params::rlimit_files,
+  $rlimit_core                 = $php_fpm::params::rlimit_core,
+  $events_mechanism            = $php_fpm::params::events_mechanism,
+) inherits php_fpm::params {
   package { 'php-fpm':
     ensure => $ensure,
   }
